f2354ea7108950f0bcae3da8f0f9976e874d5705
yuudai.kakita <kakkita_21@outlook.jp>
2023-01-21 20:58:11 +0900

    getAllTasksを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,11 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class IndexServlet
@@ -25,9 +31,13 @@ public class IndexServlet extends HttpServlet {
         /**
          * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
          */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                response.getWriter().append("Served at: ").append(request.getContextPath());
-        }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
+
+        em.close();
+    }
 
 }
